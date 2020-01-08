c1f9c9e35a0cb48bb29ad76186604797cd022ce6
Bulowl <38203107+Bulowl@users.noreply.github.com>
2020-01-08 11:11:56 +0100

    add simple exploration (#16)
    * Remove too violent push method for gitflow
    
    * Simplified collision meshes
    
    * Refactor URDF
    
    * Add robot_state_publisher to main launch
    
    * Add cave world
    
    * Add Sujet.pdf
    
    * add beamer file on Git/GitHub
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    Solved issue #2 
    Thanks @bnzr !
    
    * nom Alexndre
    
    * add corentin
    
    * add phil name
    
    * Updated README.md with my name
    
    * name
    
    * Changement Nom
    
    * 01_Goals
    
    * nom Alexndre
    
    * add corentin
    
    * Updated README.md with my name
    
    * name
    
    * contenu modifié
    
    * Update README.md
    
    * add pictures
    
    * Conneries liées à mon IDE. Faudra que je le change un jour.
    
    * readme
    
    * added real colours
    
    * added real colours (#9)
    
    * nom Alexndre
    
    * add corentin
    
    * Updated README.md with my name
    
    * name
    
    * added real colours
    
    * reports (#10)
    
    * contenu modifié
    
    * Update README.md
    
    * readme
    
    * debriefs and goals
    
    * Updated README (#11)
    
    * Update README.md
    
    * Update README.md
    
    * Updated README
    
    * creation du package projet
    
    * add simple command script
    
    Co-authored-by: RemiRigal <remi.rigal@ensta-bretagne.org>
    Co-authored-by: Joris T <joris.tillet@ensta-bretagne.org>
    Co-authored-by: AlexandreCourjaud <38203726+AlexandreCourjaud@users.noreply.github.com>
    Co-authored-by: Matthix7 <37193404+Matthix7@users.noreply.github.com>
    Co-authored-by: Corentin JEGAT <38203332+corentin-j@users.noreply.github.com>
    Co-authored-by: ymusell <57345152+ymusell@users.noreply.github.com>
    Co-authored-by: Driss92 <43435537+Driss92@users.noreply.github.com>

--- workspaceRos/src/phantomx/phantomx_gazebo/scripts/walker_demo.py
+++ workspaceRos/src/phantomx/phantomx_gazebo/scripts/walker_demo.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
 
     rospy.loginfo('Walker Demo Starting')
 
-    robot.set_walk_velocity(0.2, 0, 0)
-    rospy.sleep(3)
+    robot.set_walk_velocity(0, 0, 3)
+    rospy.sleep(1)
     robot.set_walk_velocity(1, 0, 0)
     rospy.sleep(3)
     robot.set_walk_velocity(0, 1, 0)
