022e3ecae33eeeef196fbb2ec5def2716cb31959
AlexandreCourjaud <alexandre.courjaud@ensta-bretagne.org>
2020-01-15 11:32:04 +0100

    update mapping

--- workspaceRos/src/phantomx/projet_exploration/src/mapping.py
+++ workspaceRos/src/phantomx/projet_exploration/src/mapping.py
@@ -4,7 +4,7 @@ import numpy as np
 import rospy
 from nav_msgs.msg import OccupancyGrid
 from sensor_msgs.msg import LaserScan
-
+from geometry_msgs.msg import Point
 
 map_array = np.zeros((500,500))
 map_array[:,:] = -1
@@ -14,18 +14,20 @@ distance_list = []
 
 pos_x = 0
 pos_y = 0
+cap_robot = 0
 
 def mapping():
-    global map_array, distance_list, angle_list
+    global map_array, distance_list, angle_list, pos_x, pos_y
+    print(pos_x,pos_y)
     for i in range (0,len(distance_list)):
-        xWall = int(10*distance_list[i]*np.cos(angle_list[i]))+250+pos_x  # distance (m), grid (cm)
-        yWall = int(10*distance_list[i]*np.sin(angle_list[i]))+250+pos_y
+        xWall = int(10*distance_list[i]*np.cos(angle_list[i])+250+10*pos_x)  # distance (m), grid (cm)
+        yWall = int(10*distance_list[i]*np.sin(angle_list[i])+250+10*pos_y)
         map_array[yWall,xWall] = 100
 
         rangeEmpty = np.arange(0,distance_list[i]-0.1,0.1)
         for j in range(0,len(rangeEmpty)):
-            xEmpty = int(10*rangeEmpty[j]*np.cos(angle_list[i])+250+pos_x)
-            yEmpty = int(10*rangeEmpty[j]*np.sin(angle_list[i])+250+pos_y)
+            xEmpty = int(10*rangeEmpty[j]*np.cos(angle_list[i])+250+10*pos_x)
+            yEmpty = int(10*rangeEmpty[j]*np.sin(angle_list[i])+250+10*pos_y)
             if map_array[yEmpty,xEmpty] == -1:
                 map_array[yEmpty,xEmpty] = 0
 
@@ -33,23 +35,29 @@ def mapping():
 
 def publishMap(pub_map,msg_map):
     global map_array
+    msg_map.info.origin.position.x = -25- pos_x
+    msg_map.info.origin.position.y = -25- pos_y
     msg_map.data = map_array.flatten()
     pub_map.publish(msg_map)
 
 
 def sub_lidar(msg):
     global angle_list, distance_list
-
     distance_list = msg.ranges
-    angle_list = np.arange(msg.angle_min,msg.angle_max,msg.angle_increment)
+    angle_list = np.arange(msg.angle_min-cap_robot,msg.angle_max-cap_robot,msg.angle_increment)
 
-
+def sub_position(msg):
+    global pos_x, pos_y
+    pos_x = msg.x
+    pos_y = msg.y
 
 if __name__ == "__main__":
     rospy.init_node("mapping")
 
-    rospy.Subscriber("/phantomx/scan",LaserScan,sub_lidar)
     pub_map = rospy.Publisher("/map",OccupancyGrid,queue_size=10)
+
+    rospy.Subscriber("/phantomx/scan",LaserScan,sub_lidar)
+    rospy.Subscriber("/vect_position",Point,sub_position)
     msg_map = OccupancyGrid()
 
     msg_map.header.frame_id = "base_link"
@@ -57,8 +65,8 @@ if __name__ == "__main__":
     msg_map.info.resolution = 0.1
     msg_map.info.width  = 500
     msg_map.info.height = 500
-    msg_map.info.origin.position.x = -25
-    msg_map.info.origin.position.y = -25
+    msg_map.info.origin.position.x = -25- pos_x
+    msg_map.info.origin.position.y = -25- pos_y
     msg_map.info.origin.position.z = 0
     msg_map.info.origin.orientation.x = 0
     msg_map.info.origin.orientation.y = 0
@@ -66,7 +74,7 @@ if __name__ == "__main__":
     msg_map.info.origin.orientation.w = 0
 
 
-    rate = rospy.Rate(5)
+    rate = rospy.Rate(2)
     while not rospy.is_shutdown():
         mapping()
         publishMap(pub_map,msg_map)
